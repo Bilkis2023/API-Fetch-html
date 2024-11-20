0f745b47af6da842a409fb59a43f7a25887b0a55
mst <bilkis.naher@yahoo.co.uk>
2024-11-20 08:42:54 +0000

    add the script file to the repository

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ tRow.append(titleTd, yearTd, actorsTd);
 // Append the tableRow to the tableBody
 $("tbody").append(tRow);
 };
+
+    //  ++++  
+// The below code fills in the first row of the table:
+var movie = "Dune";
+var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+fetch(queryURL)
+.then(function(response){
+    return response.json();
+}).then(function(data){
+    // Create a new table row element or a new data:
+    var tRow = $("<tr>");
+    //  Methods run on jquery selectors return selector they run on
+    // This is why we can create and save a reference to a td. In the same statement we update its text
+    var titleTd = $("<td>").text(data.Title);
+    var yearTd = $("<td").text(data.Year);
+    var actorsTd = $("<td>").text(data.Actors);
+    // Append the newly created table data in the table row
+    tRow.append(titleTd, yearTd, actorsTd);
+    // Append the table row to the tablw body
+    $("tbody").append(tRow); 
+})
+
+
+
